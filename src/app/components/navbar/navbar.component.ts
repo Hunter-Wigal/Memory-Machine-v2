c2283f3bbb6fb6660abdd0da8a06ea463c263ddd
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-07-16 13:17:18 -0400

    Add angular material to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,21 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, ElementRef, NgModule, ViewChild } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet, Router, Routes } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { User } from 'firebase/auth';
 // Temp solution
-import { NgForm, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgForm, FormsModule } from '@angular/forms'
 import { OnInit } from '@angular/core';
-import {MatToolbar} from '@angular/material/toolbar';
+import { MatToolbar } from '@angular/material/toolbar';
 import { MatAnchor } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatNavList, MatListItem } from '@angular/material/list'
+import { MatIcon } from '@angular/material/icon'
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbar, MatAnchor, FormsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbar, MatAnchor, FormsModule, MatNavList, MatListItem, MatIcon],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -21,11 +25,14 @@ export class NavbarComponent implements OnInit {
   @ViewChild("signIn") loginModal!: ElementRef;
   @ViewChild("signUp") registerUserModal!: ElementRef;
 
-  constructor(private authService: AuthService) {
+  public router: Router;
+
+  constructor(private authService: AuthService, router: Router) {
     this.currUser = this.authService.getCurrUser();
     this.authService.setUserFunc((user: User) => {
       this.currUser = user;
-    })
+    });
+    this.router = router;
   }
 
   ngOnInit() {
@@ -74,7 +81,7 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  public async signOut(){
+  public async signOut() {
     await this.authService.signOut();
     location.reload();
   }
