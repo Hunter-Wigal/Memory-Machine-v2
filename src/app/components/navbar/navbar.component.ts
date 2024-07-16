efc806480814158312c2fc9a37f91d0d0da3196c
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-07-16 14:50:26 -0400

    Update appearance of navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,16 +6,42 @@ import { User } from 'firebase/auth';
 import { NgForm, FormsModule } from '@angular/forms'
 import { OnInit } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MatAnchor } from '@angular/material/button';
+import { MatAnchor, MatButton } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatNavList, MatListItem } from '@angular/material/list'
-import { MatIcon } from '@angular/material/icon'
+import { MatIcon, MatIconRegistry } from '@angular/material/icon'
 import { NgFor } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
+
+// Used for an example of a custom icon
+// const ACCOUNT_ICON=
+// `
+// <svg 
+//   width="100" 
+//   height="100" 
+//   viewBox="0 0 24 24" 
+//   fill="none" 
+//   xmlns="http://www.w3.org/2000/svg">
+//   <!-- Circular Border -->
+//   <circle 
+//     cx="12" 
+//     cy="12" 
+//     r="11" 
+//     stroke="black" 
+//     stroke-width="2" 
+//     fill="none"/>
+  
+//   <!-- Account Icon -->
+//   <path 
+//     d="M12 2C10.067 2 8.5 3.567 8.5 5.5C8.5 7.433 10.067 9 12 9C13.933 9 15.5 7.433 15.5 5.5C15.5 3.567 13.933 2 12 2ZM12 11C9.243 11 6.801 12.216 5.312 14.073C3.934 15.805 3 17.988 3 20C3 20.553 3.447 21 4 21H20C20.553 21 21 20.553 21 20C21 17.988 20.066 15.805 18.688 14.073C17.199 12.216 14.757 11 12 11Z" 
+//     fill="black"/>
+// </svg>
+// `
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbar, MatAnchor, FormsModule, MatNavList, MatListItem, MatIcon],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbar, MatAnchor, FormsModule, MatNavList, MatListItem, MatIcon, MatButton],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -27,12 +53,14 @@ export class NavbarComponent implements OnInit {
 
   public router: Router;
 
-  constructor(private authService: AuthService, router: Router) {
+  constructor(private authService: AuthService, router: Router){ //, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     this.currUser = this.authService.getCurrUser();
     this.authService.setUserFunc((user: User) => {
       this.currUser = user;
     });
     this.router = router;
+
+    // iconRegistry.addSvgIconLiteral('account', sanitizer.bypassSecurityTrustHtml(ACCOUNT_ICON));
   }
 
   ngOnInit() {
@@ -84,5 +112,9 @@ export class NavbarComponent implements OnInit {
   public async signOut() {
     await this.authService.signOut();
     location.reload();
+  }
+
+  public accountDropdown(dropdown: HTMLElement){
+    dropdown.style.display = dropdown.style.display == "none" || dropdown.style.display == "" ? "flex" : "none";
   }
 }
