4d6ac4f26ee1120cc9c89229156544119ee56e03
Hunter-Wigal <hgw00002@mix.wvu.edu>
2024-09-10 14:18:49 -0400

    Add events table and styling to calendar page

--- src/app/pages/calendar/calendar-page.component.ts
+++ src/app/pages/calendar/calendar-page.component.ts
@@ -1,58 +1,46 @@
-import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { Component } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import {MatDatepickerModule } from '@angular/material/datepicker';
 import {MatCardModule } from '@angular/material/card';
 import { CalendarComponent } from '../../components/calendar/calendar.component';
+import { MatTableModule } from '@angular/material/table';
+import { DatePipe } from '@angular/common';
+
+
+export interface Event {
+  number: number;
+  name: string;
+  date: Date;
+  location?: string;
+  time?: string;
+}
+
+const SAMPLE_EVENTS: Event[] = [
+  { number: 1, name: 'Event ', date: new Date(), location: 'Work' },
+  { number: 2, name: 'Event ', date: new Date(), location: 'Anywhere' },
+  { number: 3, name: 'Event ', date: new Date(), location: 'Anywhere' },
+  { number: 4, name: 'Event ', date: new Date(), location: 'Home' },
+  { number: 5, name: 'Event ', date: new Date(), location: 'Anywhere' },
+  { number: 6, name: 'Event ', date: new Date(), location: 'Anywhere' },
+  { number: 7, name: 'Event ', date: new Date(), location: 'Home' },
+  { number: 8, name: 'Event ', date: new Date(), location: 'Anywhere' },
+  { number: 9, name: 'Event ', date: new Date(), location: 'Anywhere' },
+  { number: 10, name: 'Event ', date: new Date(), location: 'Site 3' },
+];
+
 @Component({
   selector: 'app-calendar-page',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatDatepickerModule, MatCardModule, CalendarComponent],
+  imports: [MatCardModule, CalendarComponent, MatTableModule, DatePipe],
   templateUrl: './calendar-page.component.html',
   styleUrl: './calendar-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CalendarPage {
 
+  displayedColumns: string[] = ['number', 'name', 'date', 'location'];
+  dataSource: Event[] | null;
 
-  // rows = [
-  //   [0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0],
-  //   [0, 0, 0, 0, 0, 0, 0],
-  // ];
-
-  // monthName = 'January';
-  // constructor() {
-  //   this.setCalendar();
-  // }
-
-  // private setCalendar() {
-  //   let today = new Date();
-
-  //   // First day of month
-  //   let firstDay = new Date(today.getFullYear(), today.getMonth());
-  //   // Last day of month
-  //   let lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-  //   // Starting index. Sunday = 0, Saturday = 6
-  //   let start = firstDay.getDay();
-  //   let end = lastDay.getDate();
-
-  //   let day = 1;
-
-  //   for (let row of this.rows) {
-  //     for (let i = start; i < 7; i++) {
-  //       if (day > end) {
-  //         break;
-  //       }
-  //       row[i] = day++;
-  //     }
-  //   }
-
-  //   this.monthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(
-  //     today
-  //   );
-  // }
+  constructor(){
+    this.dataSource = SAMPLE_EVENTS;
+  }
 }
