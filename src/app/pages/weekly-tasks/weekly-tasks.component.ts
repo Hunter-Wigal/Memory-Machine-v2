a9b7ae31f02518a2a01ef99b749d1b1d7beed785
Hunter-Wigal <hgw00002@mix.wvu.edu>
2025-03-17 17:36:00 -0400

    Start implementing dragging

--- src/app/pages/weekly-tasks/weekly-tasks.component.ts
+++ src/app/pages/weekly-tasks/weekly-tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -28,22 +28,59 @@ interface WeeklyTask{
 export class WeeklyTasksComponent {
   protected taskRow: Array<Week> = [];
   protected taskList: Array<WeeklyTask> = [];
+  protected dragging: HTMLElement | null = null;
+  protected draggingX = 0;
+  protected draggingY = 0;
 
   constructor() {
     this.taskRow.push({
-      sunday: "Do Something on this day",
-      monday: "Something",
-      tuesday: "Something",
+      sunday: 'Do Something on this day',
+      monday: 'Something',
+      tuesday: 'Something',
       wednesday: null,
-      thursday: "Something",
+      thursday: 'Something',
       friday: null,
-      saturday: "Something",
+      saturday: 'Something',
     });
 
-    this.taskList.push({taskID: "0", taskName: "Test"});
+    this.taskList.push({ taskID: '0', taskName: 'Test' });
   }
 
-  deleteTask(taskId: string){
+  deleteTask(taskId: string) {}
 
+  eventListener(event: MouseEvent) {
+    this.setElementPosition(event.clientX, event.clientY);
+    console.log(event);
+    return this;
+  }
+
+  mouseDown(event: MouseEvent, id: string) {
+    this.dragging = document.getElementById(id);
+    if(!this.dragging)
+      return
+    this.draggingX = this.dragging.getBoundingClientRect().x;
+    this.draggingY = this.dragging.getBoundingClientRect().y;
+    let width = this.dragging.getBoundingClientRect().width;
+
+    this.dragging.style.position = "absolute";
+    this.setElementPosition(this.draggingX, this.draggingY);
+    this.dragging.style.width = width + "px";
+    this.dragging.style.zIndex = '100';
+
+    console.log(this.dragging);
+    // this.dragging = event.target;
+    document.addEventListener('mousemove', ($event)=>{this.eventListener(<MouseEvent>$event)});
+  }
+
+  mouseUp(event: Event) {
+    document.removeEventListener('mousemove', ($event)=>{this.eventListener(<MouseEvent>$event);});
+  }
+
+  setElementPosition(x: number, y: number){
+    if(!this.dragging)
+      return;
+    console.log(this.dragging.style.x);
+    this.dragging.style.left = x + 'px';
+    this.dragging.style.top = y + 'px';
   }
 }
